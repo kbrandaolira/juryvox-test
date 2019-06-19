35f13a6ca80f5b1de475578be5683ff365002191
Lira, Kayan <kbrandaolira@gmail.com>
2019-06-18 22:41:45 -0300

    Criar Ação de Listar Voos #4

--- flight-project/src/Flights/Flights.js
+++ flight-project/src/Flights/Flights.js
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, {Component}  from 'react';
 
-const flights = () => {
-    return  <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>Number</th>
-                        <th>Origin</th>
-                        <th>Destination</th>
-                        <th>Departure Time</th>
-                        <th>Arrival Time</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>0001</td>
-                        <td>Rio de Janeiro</td>
-                        <td>São Paulo</td>
-                        <td>18/06/2019 8:00</td>
-                        <td>18/06/2019 8:45</td>
-                        <td><a href="#">Edit</a> / <a href="#">Remove</a></td>
-                    </tr>
-                </tbody>
-            </table>
-  
-};
+class Flights extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            error: null,
+            isLoaded: false,
+            flights: []
+        }
+    }
 
-export default flights;
+    componentDidMount() {
+        fetch("http://localhost:3000/flights")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    flights: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error: error
+                });
+            }
+        )
+    }
+
+    render() {
+        const { error, isLoaded, flights } = this.state;
+
+        if(error){
+            return <div>Error: {error.message}</div>
+        } else if(!isLoaded){
+            return <div>Loading...</div>
+        } else {
+            return <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Number</th>
+                            <th>Origin</th>
+                            <th>Destination</th>
+                            <th>Departure Time</th>
+                            <th>Arrival Time</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {flights.map(flight => (
+                            <tr>
+                                <td>{flight.number}</td>
+                                <td>{flight.origin}</td>
+                                <td>{flight.destination}</td>
+                                <td>{flight.departure_time}</td>
+                                <td>{flight.arrival_time}</td>
+                                <td><a href="#">Edit</a> / <a href="#">Delete</a></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+        }
+    }
+}
+
+export default Flights;
