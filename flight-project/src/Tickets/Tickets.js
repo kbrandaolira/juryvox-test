9aa23161cc58a831adff62743e2983796293f8a7
Lira, Kayan <kbrandaolira@gmail.com>
2019-06-18 22:57:20 -0300

    Criar Ação de Listar Tickets #Tickets #9

---
+++ flight-project/src/Tickets/Tickets.js
@@ -0,0 +1,67 @@
+import React, {Component}  from 'react';
+
+class Tickets extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            error: null,
+            isLoaded: false,
+            tickets: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3000/tickets")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                    passengers: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error: error
+                });
+            }
+        )
+    }
+
+    render() {
+        const { error, isLoaded, tickets } = this.state;
+
+        if(error){
+            return <div>Error: {error.message}</div>
+        } else if(!isLoaded){
+            return <div>Loading...</div>
+        } else {
+            return <div>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>National ID</th>
+                                    <th>Flight Number</th>
+                                    <th>Seat</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {tickets.map(ticket => (
+                                    <tr>
+                                        <td>{ticket.national_id}</td>
+                                        <td>{ticket.flight_number}</td>
+                                        <td>{ticket.seat}</td>
+                                        <td><a href="#">Edit</a> / <a href="#">Delete</a></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <input className="btn btn-success new-btn" value="New Ticket" type="button"/>
+                    </div>
+        }
+    }
+}
+
+export default Tickets;
